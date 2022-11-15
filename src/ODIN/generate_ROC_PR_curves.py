747ef92f2c968a31b8bd16b09007bbb05b0d2486
harryli0088 <harryli0088@gmail.com>
2022-11-15 12:10:49 -0500

    added PR curves and code

--- src/ODIN/generate_ROC_PR_curves.py
+++ src/ODIN/generate_ROC_PR_curves.py
@@ -5,42 +5,70 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import sklearn.metrics
+from sklearn.metrics import auc, precision_recall_curve, roc_curve
 
-PARENT_DIRECTORY = "./Train_CIFAR-10_Test_Imagenet/" # set the parent directory of the text file results
-TITLE = "ODIN vs Baseline - Train CIFAR-10, Test Imagenet - " # title of the plots
+PARENT_DIRECTORY = "./Train_CIFAR-10_Test_SVHN/" # set the parent directory of the text file results
+TITLE = "ODIN vs Baseline - Train CIFAR-10, Test SVHN - " # title of the plots
 
 baseline_ID_data = np.loadtxt(PARENT_DIRECTORY+'confidence_Base_In.txt', delimiter=',')
 baseline_OOD_data = np.loadtxt(PARENT_DIRECTORY+'confidence_Base_Out.txt', delimiter=',')
 ODIN_ID_data = np.loadtxt(PARENT_DIRECTORY+'confidence_Our_In.txt', delimiter=',')
 ODIN_OOD_data = np.loadtxt(PARENT_DIRECTORY+'confidence_Our_Out.txt', delimiter=',')
 
-def generate_ROC_curve(ID_data, OOD_data, name=""):
+def calc_ROC_PR_data(ID_data, OOD_data):
     ID_probabilities = ID_data[:,2:]
     OOD_probabilities = OOD_data[:,2:]
     ID_labels = np.ones_like(ID_probabilities)
     OOD_labels = np.zeros_like(OOD_probabilities)
 
     # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.RocCurveDisplay.html
-    (fpr,tpr,thresholds) = sklearn.metrics.roc_curve(
+    (fpr,tpr,thresholds) = roc_curve(
         np.concatenate((ID_labels, OOD_labels),axis=0),
         np.concatenate((ID_probabilities, OOD_probabilities),axis=0),
     )
 
-    roc_auc = sklearn.metrics.auc(fpr, tpr)
+    (precision,recall,thresholds) = precision_recall_curve(
+        np.concatenate((ID_labels, OOD_labels),axis=0),
+        np.concatenate((ID_probabilities, OOD_probabilities),axis=0),
+    )
+
+    roc_auc = auc(fpr, tpr)
+    pr_auc = auc(recall, precision)
+
 
     # display = sklearn.metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc, estimator_name=name)
     # display.plot()
 
-    # https://stackoverflow.com/questions/42894871/how-to-plot-multiple-roc-curves-in-one-plot-with-legend-and-auc-scores-in-python
-    plt.plot(fpr,tpr,label="{}, AUROC={:.3f}".format(name,roc_auc))
+    return fpr, tpr, roc_auc, precision, recall, pr_auc
+    
+(
+  baseline_fpr, baseline_tpr, baseline_auroc,
+  baseline_precision, baseline_recall, baseline_aupr
+) = calc_ROC_PR_data(baseline_ID_data, baseline_OOD_data)
+(
+  ODIN_fpr, ODIN_tpr, ODIN_auroc,
+  ODIN_precision, ODIN_recall, ODIN_aupr
+) = calc_ROC_PR_data(ODIN_ID_data, ODIN_OOD_data)
 
-plt.figure(0).clf()
-
-generate_ROC_curve(baseline_ID_data,baseline_OOD_data,"baseline")
-generate_ROC_curve(ODIN_ID_data,ODIN_OOD_data,"ODIN")
-
+plt.figure(1).clf()
 plt.title(TITLE + "ROC Curves")
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.plot(baseline_fpr,baseline_tpr,label="Baseline, AUROC={:.3f}".format(baseline_auroc))
+plt.plot(ODIN_fpr,ODIN_tpr,label="ODIN, AUROC={:.3f}".format(ODIN_auroc))
 plt.legend(loc=0)
 plt.savefig(PARENT_DIRECTORY+'roc_curve.png')
+
+plt.figure(2).clf()
+plt.title(TITLE + "PR Curves")
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.plot(baseline_precision,baseline_recall,label="Baseline, AUPR={:.3f}".format(baseline_aupr))
+plt.plot(ODIN_precision,ODIN_recall,label="ODIN, AUPR={:.3f}".format(ODIN_aupr))
+plt.legend(loc='best')
+plt.savefig(PARENT_DIRECTORY+'pr_curve.png')
+
+
+
+
 plt.show()
