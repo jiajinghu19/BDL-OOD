faa3d1b431f34a1cad4aa6f2ebe74d2cd7fe8087
harryli0088 <harryli0088@gmail.com>
2022-12-01 10:12:05 -0500

    evaluate train MNIST test Fashion MNIST

---
+++ src/LMPBT/results/generate_samples.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+
+import torch
+import torch.optim
+import matplotlib.pyplot as plt
+
+from torchvision.utils import save_image
+
+import DCGAN_VAE_pixel as DVAE
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+ngpu = 0
+nz = 100
+ngf = 64
+nc = 3
+image_size = 32
+state_G = "../LMPBT_fork/cifar100_netG.pth"
+
+netG = DVAE.DCGAN_G(image_size, nz, nc, ngf, ngpu)
+state_G = torch.load(state_G, map_location=device)
+netG.load_state_dict(state_G, strict=False)
+
+# netE = DVAE.Encoder(image_size, nz, nc, ngf, ngpu)
+# state_E = torch.load(state_E, map_location=device)
+# netE.load_state_dict(state_E, strict=False)
+
+netG.to(device)
+netG.eval()
+# netE.to(device)
+# netE.eval()
+
+with torch.no_grad():
+    sample = torch.randn(1, nz, 1, 1).to(device) # this is correct
+    sample = netG(sample)
+    print("sample.size()",sample.size())
+    print("test sample size??",sample[0,0,0,0].size)
+    print("test sample??",sample[0,0,0,0])
+    # print("sample",torch.mean(sample,4).size())
+    save_image(
+        sample[0,:,:,:,:].view(256,nc,image_size,image_size), 
+        'test.png',
+        nrow=32, 
+        padding=0
+    )
